697c5a47541f023a2eb3eef99a5f41edffa8d7f4
Yue JIN <yjin@nustarnuclear.com>
2024-06-13 14:13:43 +0800

    feat: allow use both renderers

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,13 @@ import type * as echartsFeatures from 'echarts/features'
 type ComponentName = keyof typeof echartsComponents
 type ChartName = keyof typeof echartsCharts
 type FeaturesName = keyof typeof echartsFeatures
+type RendererName = 'canvas' | 'svg'
 
 export interface ModuleOptions {
   /**
    * @default 'canvas'
    */
-  renderer?: 'canvas' | 'svg'
+  renderer?: RendererName | RendererName[]
 
   /**
    * Register the required charts globally

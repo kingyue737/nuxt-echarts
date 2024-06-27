81ee8e72e0a79df2fa5f154a1bc591ed2efab958
Yue JIN <yjin@nustarnuclear.com>
2024-06-27 17:35:43 +0800

    test: add basic e2e test

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,5 +1,22 @@
-<template>
-  <div>basic</div>
-</template>
+<script setup lang="ts">
+const option = {
+  series: [
+    {
+      type: 'pie',
+      roseType: 'angle',
+      data: [
+        { value: 800, name: 'JIN' },
+        { value: 735, name: 'Yue' },
+      ],
+    },
+  ],
+}
+const clicked = ref(false)
+</script>
 
-<script setup></script>
+<template>
+  <div>
+    <VChartLight :init-options="{ height: 300, width: 300 }" :option="option" />
+    <div>{{ clicked }}</div>
+  </div>
+</template>
